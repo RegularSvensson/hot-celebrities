b642575d785db8154d8894aacc13e341eda33523
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-09 14:05:09 +0200

    Add Model

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-app.js
+
+/* ======= Model ======= */
+var initialCelebrities = [
+	{
+		clickCount: 0,
+		name: 'Scarlett Johansson',
+		imgSrc: 'img/scarjo.jpg',
+		nicknames: ['ScarJo', 'blondie']
+	},
+	{
+		clickCount: 0, 
+		name: 'Jessica Biel',
+		imgSrc: 'img/jessbiel.jpg',
+		nicknames: ['Jess', 'Bielsy']
+	}
+]
