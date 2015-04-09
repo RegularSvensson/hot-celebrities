53df4178ce3b87cbcc0cdd8808feba2cd53c4783
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-09 14:05:22 +0200

    Add ViewModel

--- js/app.js
+++ js/app.js
@@ -14,3 +14,25 @@ var initialCelebrities = [
 		nicknames: ['Jess', 'Bielsy']
 	}
 ]
+
+/* ======= ViewModel (Octopus) ======= */
+
+var ViewModel = function() {
+	var self = this;
+
+	this.celebrityList = ko.observableArray([]);
+
+	initialCelebrities.forEach(function(celebrityItem) {
+		self.celebrityList.push( new Celebrity(celebrityItem) );
+	});
+
+	this.currentCelebrity = ko.observable( this.celebrityList()[0] );
+
+	this.incrementCounter = function() {
+		self.currentCelebrity().clickCount( self.currentCelebrity().clickCount() + 1);
+	};
+
+	this.setCelebrity = function(clickedCelebrity) {
+		self.currentCelebrity(clickedCelebrity);
+	};
+}
